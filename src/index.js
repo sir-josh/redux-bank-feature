2e9fecdef11a874dfc3ecfcd8a44a4ce84644691
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2023-10-26 21:51:10 +0100

    refactor: Re-structure file setup and create state slices

--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
 import "./index.css";
 import App from "./App";
+import store from "./store";
+import { createCustomer } from "./features/customers/customerSlice";
+import {
+	deposit,
+	withdraw,
+	requestLoan,
+	payLoan,
+} from "./features/accounts/accountSlice";
 
-import "./store";
+// For Account Reducer
+store.dispatch(deposit(500));
+console.log("deposit action:", store.getState());
+
+store.dispatch(withdraw(150));
+console.log("withdraw action:", store.getState());
+
+store.dispatch(requestLoan(1500, "Pay for rental"));
+console.log("loan request action:", store.getState());
+
+store.dispatch(payLoan());
+console.log("pay loan action:", store.getState());
+
+// For Customer Reducer
+store.dispatch(createCustomer("Ikelugo Slyvester", "83920393GK"));
+console.log("create customer:", store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
