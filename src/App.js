7b31cea0360901bc3e71d4c65a1b656871f91abd
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2023-10-27 03:25:42 +0100

    feat: dispatch action for the app

--- src/App.js
+++ src/App.js
@@ -2,17 +2,25 @@ import CreateCustomer from "./features/customers/CreateCustomer";
 import Customer from "./features/customers//Customer";
 import AccountOperations from "./features/accounts/AccountOperations";
 import BalanceDisplay from "./features/accounts/BalanceDisplay";
+import { useSelector } from "react-redux";
 
 function App() {
-  return (
-    <div>
-      <h1>🏦 The React-Redux Bank ⚛️</h1>
-      <CreateCustomer />
-      <Customer />
-      <AccountOperations />
-      <BalanceDisplay />
-    </div>
-  );
+	const customer = useSelector((store) => store.customer.fullName);
+	return (
+		<div>
+			<h1>🏦 The React-Redux Bank ⚛️</h1>
+			<br />
+			{!customer ? (
+				<CreateCustomer />
+			) : (
+				<>
+					<Customer />
+					<AccountOperations />
+					<BalanceDisplay />
+				</>
+			)}
+		</div>
+	);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,30 +5,30 @@ import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
 import store from "./store";
-import { createCustomer } from "./features/customers/customerSlice";
-import {
-	deposit,
-	withdraw,
-	requestLoan,
-	payLoan,
-} from "./features/accounts/accountSlice";
+// import { createCustomer } from "./features/customers/customerSlice";
+// import {
+// 	deposit,
+// 	withdraw,
+// 	requestLoan,
+// 	payLoan,
+// } from "./features/accounts/accountSlice";
 
-// For Account Reducer
-store.dispatch(deposit(500));
-console.log("deposit action:", store.getState());
+// // For Account Reducer
+// store.dispatch(deposit(500));
+// console.log("deposit action:", store.getState());
 
-store.dispatch(withdraw(150));
-console.log("withdraw action:", store.getState());
+// store.dispatch(withdraw(150));
+// console.log("withdraw action:", store.getState());
 
-store.dispatch(requestLoan(1500, "Pay for rental"));
-console.log("loan request action:", store.getState());
+// store.dispatch(requestLoan(1500, "Pay for rental"));
+// console.log("loan request action:", store.getState());
 
-store.dispatch(payLoan());
-console.log("pay loan action:", store.getState());
+// store.dispatch(payLoan());
+// console.log("pay loan action:", store.getState());
 
-// For Customer Reducer
-store.dispatch(createCustomer("Ikelugo Slyvester", "83920393GK"));
-console.log("create customer:", store.getState());
+// // For Customer Reducer
+// store.dispatch(createCustomer("Ikelugo Slyvester", "83920393GK"));
+// console.log("create customer:", store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
